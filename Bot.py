1c53e92f5ed396325bdeb1e5624a495f06465b91
Marco Andronaco <andronacomarco@gmail.com>
2023-06-20 01:49:56 +0200

    add telegram functionality

---
+++ Bot.py
@@ -0,0 +1,48 @@
+import requests, logging, json
+from time import sleep
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+REQUEST_TIMEOUT = 3
+MAX_RETRIES = 3
+BASE_POST_URL = "https://gelbooru.com/index.php?page=post&s=view&id="
+
+def telegram_request(payload, post, token, method="sendMessage"):
+    
+    i = 0
+    while i < MAX_RETRIES:
+        request_url = 'https://api.telegram.org/bot{0}/{1}'.format(token, method)
+        r = requests.post(url=request_url, data=payload).json()
+        
+        if r['ok']:
+            return r
+        i += 1
+        logging.debug(f"{i}: {payload}")
+        logging.warn(f"{i}: {r}")
+        sleep(REQUEST_TIMEOUT)
+    
+    return telegram_request({
+                            'chat_id': payload['chat_id'],
+                            'text': f"[Failed]({BASE_POST_URL}{post['id']}).",
+                            'parse_mode': 'markdown'
+                            }, post=post, token=token) 
+
+def send_post_telegram(post, bot_token, chat_id):
+    
+    extension = post["filename"].split(".")[-1]
+    link = post["url"]
+    reply_markup = json.dumps({ "inline_keyboard": [[{ "text": 'Sauce 🍝', "url": BASE_POST_URL + str(post['id']) }]]})
+    
+    r = None
+    match extension:
+        case "jpeg" | "jpg" | "png":
+            r = telegram_request({ 'chat_id': chat_id, 'photo': link, 'reply_markup': reply_markup },
+                                 post=post, token=bot_token, method="sendPhoto")
+        case "gif" | "mp4":
+            r = telegram_request({ 'chat_id': chat_id, 'video': link, 'reply_markup': reply_markup },
+                                 post=post, token=bot_token, method="sendVideo")
+        case _:
+            logging.debug(f"Unhandled extension {extension} for link: {link}.")
+            r = telegram_request({ 'chat_id': chat_id, 'text': f"[Unknown extension]({link}).", 'parse_mode': 'markdown' },
+                                 post=post, token=bot_token)
+    return r
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from pygelbooru import Gelbooru
-from time import sleep, mktime
-import requests, json, asyncio
+from time import sleep
 from datetime import datetime
+from Bot import send_post_telegram
+import requests, json, asyncio, logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S%z"
 ALL_POSTS_PATH = "all_posts.json"
@@ -9,33 +11,26 @@ SENT_POSTS_PATH = "sent_posts.json"
 LIMIT = 100
 REFRESH_DELAY = 60 * 60 # 1h
 GELBOORU_DELAY = 3
-SEND_DELAY = 60 * 3 # 5m
-
-async def get_posts_from_author(author: str):
+SEND_DELAY = 60 * 3 # 3m
     
-    print("Getting posts from", author)
-    res = []
+async def get_all_posts():
+
+    logging.debug("Config:" + str(config))
     gelbooru = Gelbooru(config["gelbooru_api_key"], config["gelbooru_user_id"])
     
+    posts = []
     i = 0
     while True:
-        temp_results = await gelbooru.search_posts(tags=[author], page=i, limit=LIMIT)
-        res += temp_results
+        logging.debug(f"Asking for page {i}.")
+        temp_results = await gelbooru.search_posts(tags=config["tags"], page=i, limit=LIMIT)
+        posts += temp_results
         
         if len(temp_results) < LIMIT:
             break
         i += 1
         sleep(GELBOORU_DELAY)
     
-    return res
-    
-async def get_all_posts():
-    posts = []
-    
-    for x in config["authors"]:
-        posts += await get_posts_from_author(x)
-    
-    print("Got", len(posts), "posts") # TODO: use logging library
+    logging.info(f"Got {len(posts)} posts.")
     
     posts_info = []
     for post in posts:
@@ -48,31 +43,16 @@ async def get_all_posts():
             "rating": post.rating,
             "source": post.source
         })
-    
-    posts = sorted(posts_info, key=lambda x:x["created"], reverse=False)
-    
-    with open(ALL_POSTS_PATH, "w", encoding="utf-8") as out_file:
-        out_file.write(json.dumps(posts))
         
-    return posts
-
-def load_all_posts():
-    with open(ALL_POSTS_PATH, "r", encoding="utf-8") as in_file:
-        posts = json.loads(in_file.readline())
-        
-    return posts
+    return sorted(posts_info, key=lambda x:x["created"], reverse=False)
 
 def save_sent_ids(input_list):
+    
     with open(SENT_POSTS_PATH, "w", encoding="utf-8") as out_file:
         out_file.write(json.dumps(input_list))
     return len(input_list)
 
 def send_posts(posts, safe_update=False):
-    try:
-        with open(ALL_POSTS_PATH, "r", encoding="utf-8") as in_file:
-            posts = json.loads(in_file.readline())
-    except FileNotFoundError:
-        return
     
     try:
         with open(SENT_POSTS_PATH, "r", encoding="utf-8") as in_file:
@@ -85,7 +65,8 @@ def send_posts(posts, safe_update=False):
         i += 1
         post_id = post["id"]
         if post_id not in sent_ids:
-            print(i, "SENDING POST ID:", post_id, "creator_id:", post["creator"])
+            logging.debug(f'{i}: SENDING POST ID {post_id}, CREATOR: {post["creator"]}')
+            send_post_telegram(post, bot_token, chat_id)
             sent_ids.append(post_id)
             
             if safe_update:
@@ -98,18 +79,29 @@ def send_posts(posts, safe_update=False):
 
 ## BEGIN COROUTINES FOR ASYNCIO
 async def get_posts_coroutine():
+    
     while True:
         posts = await get_all_posts()
         sent = send_posts(posts, safe_update=True)
         
-        print(f"get_posts_coroutine(): {len(posts)} post di cui {len(sent)} inviati.")
+        logging.debug(f"get_posts_coroutine(): {len(posts)} post di cui {len(sent)} inviati.")
         sleep(REFRESH_DELAY)
 
 if __name__ == "__main__":
-    with open("config.json", "r", encoding="utf-8") as in_file:
-        config = json.loads("".join(in_file.readlines()))
+    
+    try:
+        with open("config.json", "r", encoding="utf-8") as in_file:
+            config = json.loads("".join(in_file.readlines()))
+    except FileNotFoundError:
+        logging.error("Please create a file named config.json in this folder.")
+        exit(1)
         
-    
+    chat_id = config["chat_id"]
+    bot_token = config["bot_token"]
+    if chat_id == "" or bot_token == "":
+        logging.error("Please check your config.json.")
+        exit(1)
+        
     loop = asyncio.get_event_loop()
-    
     loop.run_until_complete(get_posts_coroutine())
+    print("Skekeke")
